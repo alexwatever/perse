18bd955f132222b37e02aa54b5e81810063d3ee7
Alex Watson <alex@coded.cc>
2023-09-29 21:08:51 +1000

    Setup a test Postgres connection, yet untested. This work will likely become Perse_Data.

---
+++ perse/src/db.rs
@@ -0,0 +1,67 @@
+
+pub type DatabasePool = sqlx::Pool<sqlx::Postgres>;
+pub static DATABASE_POOL: once_cell::sync::OnceCell<DatabasePool> = once_cell::sync::OnceCell::new();
+
+pub struct Database {}
+impl Database {
+    /// Initialise the database connection pool
+    #[cfg(feature = "ssr")]
+    pub async fn initialise() -> Result<DatabasePool, sqlx::Error> {
+        // Create a new PostgreSQL database connection pool
+        dotenv::dotenv().ok();
+        Self::create_connection_pool(
+            std::env::var("PERSE_DATABASE_URL")
+                .unwrap_or_else(|_| "".to_string()),
+            std::env::var("PERSE_DATABASE_MAX_CONNECTIONS")
+                .unwrap_or_else(|_| "".to_string())
+                .parse::<u32>()
+                .unwrap_or_else(|_| 0_u32),
+        )
+        .await
+    }
+
+    /// Get the existing database pool
+    #[cfg(feature = "ssr")]
+    pub fn get_connection_pool() -> Option<&'static DatabasePool> {
+        super::db::DATABASE_POOL.get()
+    }
+
+    /// Create the initial database connection pool
+    #[cfg(feature = "ssr")]
+    async fn create_connection_pool(database_url: String, max_connections: u32) -> Result<DatabasePool, sqlx::Error> {
+        // Setup a new PostgreSQL database connection pool with provided configuration
+        sqlx::postgres::PgPoolOptions::new()
+            .max_connections(max_connections)
+            .connect(&database_url)
+            .await
+    }
+    
+    // /// Get a database connection from the database pool
+    // #[cfg(feature = "ssr")]
+    // pub async fn get_connection() -> DatabaseConnection {
+    //     DatabaseConnection(DATABASE_POOL
+    //         .get()
+    //         .expect("Unable to retrieve the database connection pool reference.")
+    //         .clone()
+    //         .acquire()
+    //         .await
+    //         .expect("Unable to retrieve a database connection.")
+    //     )
+    // }
+}
+
+// /// Implement Dereference for DatabaseConnections, if required by sqlx/postgres (yet to be validated)
+// pub struct DatabaseConnections(pub sqlx::Pool<sqlx::Postgres>);
+// impl std::ops::Deref for DatabaseConnections {
+//     type Target = sqlx::Pool<sqlx::Postgres>;
+//     #[cfg(feature = "ssr")]
+//     fn deref(&self) -> &Self::Target {
+//         &self
+//     }
+// }
+// impl std::ops::DerefMut for DatabaseConnections {
+//     #[cfg(feature = "ssr")]
+//     fn deref_mut(&mut self) -> &mut Self::Target {
+//         self
+//     }
+// }
--- perse/src/main.rs
+++ perse/src/main.rs
@@ -1,51 +1,93 @@
+mod db;
+
+/// # Perse
 #[cfg(feature = "ssr")]
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    info!("Launching Perse...");
     use actix_files::Files;
     use actix_web::*;
     use leptos::*;
     use leptos_actix::{generate_route_list, LeptosRoutes};
+    use tracing::info;
     use perse::app::*;
 
+    // Initialising the Database connection
+    info!("Initialising the Database connection...");
+    let db: &db::DatabasePool;
+    match db::Database::initialise().await {
+        Ok(result) => {
+            // Allocate the Database connection pool reference
+            db::DATABASE_POOL
+                .set(result)
+                .expect("The database connection pool could not be created.");
+
+            // Retrieve the Database connection pool
+            db = db::Database::get_connection_pool()
+                .expect("The database connection pool could not be retrieved.");
+        },
+        Err(err) => panic!("The database failed to initialise: \n{:#?}", err)
+    }
+
+    // Check and run Database Migrations
+    info!("Checking for Database migrations...");
+    sqlx::migrate!()
+        .run(db)
+        .await
+        .expect("Unable to run the database migrations.");
+
+    // Get Leptos Configuration
+    info!("Getting the Web Server configuration...");
     let conf = get_configuration(None).await.unwrap();
     let addr = conf.leptos_options.site_addr;
-    // Generate the list of routes in your Leptos App
-    let routes = generate_route_list(|cx| view! { cx, <App/> });
 
+    // Start Web Server
     HttpServer::new(move || {
         let leptos_options = &conf.leptos_options;
         let site_root = &leptos_options.site_root;
-
+        info!("Starting Perse!");
         App::new()
+            // setup the server functions
             .route("/api/{tail:.*}", leptos_actix::handle_server_fns())
             // serve JS/WASM/CSS from `pkg`
             .service(Files::new("/pkg", format!("{site_root}/pkg")))
-            // serve other assets from the `assets` directory
+            // serve other Assets from the `assets` directory
             .service(Files::new("/assets", site_root))
-            // serve the favicon from /favicon.ico
+            // serve the Favicon from /favicon.ico
             .service(favicon)
+            // set the Routes
             .leptos_routes(
                 leptos_options.to_owned(),
-                routes.to_owned(),
+                generate_route_list(|cx| view! { cx, <App/> }),
                 |cx| view! { cx, <App/> },
             )
+            // set the Application State
             .app_data(web::Data::new(leptos_options.to_owned()))
-        //.wrap(middleware::Compress::default())
+            .app_data(web::Data::new(PerseState {
+                db,
+                // env: Configuration::init(),
+            }))
+            //.wrap(middleware::Compress::default())
     })
     .bind(&addr)?
     .run()
     .await
 }
 
+/// # Application State
+pub struct PerseState<'a> {
+    db: &'a db::DatabasePool,
+    // env: Configuration,
+}
+
+/// # Favicon Configuration
 #[cfg(feature = "ssr")]
 #[actix_web::get("favicon.ico")]
 async fn favicon(
     leptos_options: actix_web::web::Data<leptos::LeptosOptions>,
 ) -> actix_web::Result<actix_files::NamedFile> {
-    let leptos_options = leptos_options.into_inner();
-    let site_root = &leptos_options.site_root;
     Ok(actix_files::NamedFile::open(format!(
-        "{site_root}/favicon.ico"
+        "{}/favicon.ico", &leptos_options.into_inner().site_root
     ))?)
 }
 
