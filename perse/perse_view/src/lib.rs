36d516bf26da2666933dc44e23e0fbc34a1b3d12
Alex Watson <alex@coded.cc>
2024-06-07 23:00:26 +1000

    Setup ActionForm for create vie

--- perse/perse_view/src/lib.rs
+++ perse/perse_view/src/lib.rs
@@ -1,12 +1,16 @@
 use leptos::*;
+use leptos_router::ActionForm;
 
 /// # Perse View
 
-/// ## Create View
+/// ## View for "Create View"
 #[component]
 pub fn Create() -> impl IntoView {
-    let app_name: &str = "perse";
+    // Set the API server function
+    let create_view = create_server_action::<CreateView>();
 
+    // Content
+    let app_name: &str = "perse";
     view! {
         <nav id="navbar">
             <a id="brand-link" href="/" aria-label=app_name><strong>{ format!("{name}", name = app_name ) }</strong></a>
@@ -18,27 +22,27 @@ pub fn Create() -> impl IntoView {
             </header>
 
             <main>
-                <form>
+                <ActionForm action=create_view>
                     <div>
                         <div>
                             <label for="title">"Title"</label>
-                            <input id="title" type="text" placeholder="About Me" />
+                            <input id="title" name="title" type="text" placeholder="About Me" />
                         </div>
                         <div>
                             <label for="content_body">"Body Content"</label>
-                            <textarea id="content_body" placeholder="It was a bright cold day in April..."></textarea>
+                            <textarea id="content_body" name="content_body" placeholder="It was a bright cold day in April..."></textarea>
                         </div>
                         <div>
                             <label for="content_head">"Head Content"</label>
-                            <textarea id="content_head" placeholder=""></textarea>
+                            <textarea id="content_head" name="content_head" placeholder=""></textarea>
                         </div>
                         <div>
                             <label for="description">"Description"</label>
-                            <textarea id="description" placeholder=""></textarea>
+                            <textarea id="description" name="description" placeholder=""></textarea>
                         </div>
                         <div>
                             <label for="content">"Keywords"</label>
-                            <input id="keywords" type="text" placeholder="" />
+                            <input id="keywords" name="keywords" type="text" placeholder="" />
                         </div>
                         <br /><br />
                     </div>
@@ -46,7 +50,7 @@ pub fn Create() -> impl IntoView {
                     <div>
                         <div>
                             <label for="visibility">"Visibility"</label>
-                            <select id="visibility">
+                            <select id="visibility" name="visibility">
                                 <option value="visibility_public">"Public"</option>
                                 <option value="visibility_hidden">"Hidden"</option>
                                 <option value="visibility_unlisted">"Unlisted"</option>
@@ -54,10 +58,10 @@ pub fn Create() -> impl IntoView {
                         </div>
                         <div>
                             <label for="route">"Route"</label>
-                            <input id="route" type="text" placeholder="about-me" />
+                            <input id="route" name="route" type="text" placeholder="about-me" />
 
                             <div>
-                                <input id="route-checkbox" type="checkbox" />
+                                <input id="automatic_title" name="automatic_title" type="checkbox" />
                                 <label for="route-checkbox">"Create from the "<strong>"Title"</strong>" automatically"</label>
                             </div>
                         </div>
@@ -67,10 +71,28 @@ pub fn Create() -> impl IntoView {
                     <div>
                         <button type="submit" aria-label="Save View">"Save"</button>
                     </div>
-                </form>
+                </ActionForm>
             </main>
         </article>
     }
+}
+
+/// ### API for "Create View"
+#[server(name = CreateView, prefix = "/api/v1", endpoint = "view/create")]
+pub async fn create_view(
+    title: Option<String>,
+    content_body: Option<String>,
+    content_head: Option<String>,
+    description: Option<String>,
+    keywords: Option<String>,
+    visibility: Option<String>,
+    route: Option<String>,
+    automatic_title: Option<String>,
+) -> Result<String, ServerFnError> {
+    // print results
+    let results: String = format!("{title:?}, {content_body:?}, {content_head:?}, {description:?}, {keywords:?}, {visibility:?}, {route:?}, {automatic_title:?}");
+
+    Ok(results)
 }
 
 /// ## Not Found (404)
